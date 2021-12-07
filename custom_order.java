910ad74828272b4f8cedc861f7e37137b6f60797
brielle-codes <argyletady@gmail.com>
2021-12-07 03:43:32 -0500

    add cupcake shopb

--- custom_order.java
+++ custom_order.java
@@ -7,16 +7,30 @@ public class custom_order {
     // STEP 1 PRINTING HELLO WORLD TO CONSOLE
     public static void main(String[] args) {
         
-      System.out.println("Hello World!"); // print Hello World to console
+      //System.out.println("Recompiled: Hello World!"); // print Hello World to console
         
     // TEST CODE
     
     // STEP 2 CREATE A SCANNER OBEJCT, DECLARE VARAIBLES, & PRINT STATEMENTS
+    	Scanner keyboard = new Scanner (System.in);
+
+    	String firstName = ""; // user's first name
+    	String itemOrder = ""; // item ordered
+    	String frostingType = ""; // frosting ordered
+    	String fillingType = ""; // filling ordered
+    	String toppings; // toppings ordered
+    	String input; // user input
+
+    	double cost = 15.00; // cost of cake and cupcakes
+    	final double TAX_RATE = .08; // sales tax rate
+    	double tax; // amount of tax
+
 	  
 
 	 
     // Introduce shop and prompt user to input first name
-      
+    	System.out.println("Welcome to Java's Cake & Cupcake Shop!");
+    	System.out.println("We make custom cakes with our secret cake batter!");
 
 	  
 	  
@@ -62,6 +76,9 @@ public class custom_order {
     // TEST CODE
       
     // STEP 8 PROMPT USER TO CHOOSE TOPPINGS
+    	System.out.println("What type of TOPPINGS do you want?");
+    	System.out.println("Sprinkles, Cinnamon, Cocoa, Nuts");
+    	toppings = keyboard.nextLine();
 
 
 	
@@ -70,8 +87,14 @@ public class custom_order {
     // TEST CODE
       
     // STEP 9 DISPLAY ORDER CONFIRMATION
-
-
+    	System.out.println();
+    	System.out.println(firstName + " , your order is as follows: ");
+    	System.out.println("________________________________");
+    	System.out.println("Item Ordered: " + itemOrder);
+    	System.out.println("Frosting: " + frostingType);
+    	System.out.println("Filling: " + fillingType);
+    	System.out.println("Toppings: " + toppings);
+    	System.out.println("________________________________");
       
 	  
 	  
@@ -80,6 +103,10 @@ public class custom_order {
     // TEST CODE
       
     // STEP 10 DISPLAY COST AND SALES TAX
+    	System.out.printf("The cost of your order is: $%.2f\n", cost);
+    	tax = cost*TAX_RATE;
+    	System.out.printf("The teax os: $%f\n", tax);
+    	System.out.printf("The total due is: $%.2f\n", (tax + cost));
   
     }   
 }
